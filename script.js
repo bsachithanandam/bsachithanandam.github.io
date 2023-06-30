0dad6ba235d51d59f05f151247ca576b673436b7
bsachithanandam <sashibharkavi@gmail.com>
2023-06-29 22:29:16 -0700

    Changes

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ ScrollReveal().reveal('.home-content h1,.about-img',{origin:'left'});
 ScrollReveal().reveal('.home-content p,.about-content',{origin:'right'});
 
 const typed = new Typed('.multiple-text',{
-    strings:['CSE Student at NMIT ','Frontend developer'],
+    strings:['Graduate Student','Software Engineer','Computer Scientist', 'Machine Learning Engineer','AI Enthusiast','AI Researcher','Graduate Student - Computer Science'],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
