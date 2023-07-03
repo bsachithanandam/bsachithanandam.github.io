b7208a17f44b389c6846e2f66923ae73c614c511
bsachithanandam <sashibharkavi@gmail.com>
2023-07-02 18:44:11 -0700

    Updates

--- script.js
+++ script.js
@@ -51,3 +51,19 @@ const typed = new Typed('.multiple-text',{
     backDelay:1000,
     loop:true
 });
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
